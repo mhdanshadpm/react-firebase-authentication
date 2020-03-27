5e82da84c8a5f22c7b3ded9a6e1f98701f1e65bd
Muhammad Anshad <muhammad.anshad@teronext.com>
2020-03-27 15:34:40 +0530

    Sign Up page creation

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,5 @@
 import app from 'firebase/app';
 import 'firebase/auth';
-
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -8,7 +7,17 @@ const config = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-};
+    };
+// const config = {
+//     apiKey: "AIzaSyCmpiqvzOHjdXq5kdh3R8S7L-TnNSTq28c",
+//     authDomain: "anshad-react-firebase-project.firebaseapp.com",
+//     databaseURL: "https://anshad-react-firebase-project.firebaseio.com",
+//     projectId: "anshad-react-firebase-project",
+//     storageBucket: "anshad-react-firebase-project.appspot.com",
+//     messagingSenderId: "479213680704",
+//     appId: "1:479213680704:web:c1996331d535cc28d5c7a5",
+//     measurementId: "G-PS9GLLMZSQ"
+//   };
 
 class Firebase {
     constructor() {
